f72792be767eceace1834160b16515ead04e7623
Martin Lobell <martin.lobell@gmail.com>
2020-11-04 11:21:02 +0100

    Replacing first commit
    Pushed the wrong files in the first commit. Pushing the right ones instead.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Lånad funktion nedan, "Fade-in-vid-scroll-animation"
+// Hittad funktion nedan, "Fade-in-vid-scroll-animation"
 
 $(document).ready(function () {
     $(window).scroll(function () {
@@ -13,28 +13,6 @@ $(document).ready(function () {
         });
     });
 });
-
-function showLinks() {
-    var x = document.getElementById("links");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-        x.style.transition = "0.6s";
-        x.style.right = "0";
-    }
-}
-
-
-// To do, göm det engelska innehållet vid start och aktivera om knappen/länken trycks på.
-function switchLang() {
-    var x = document.getElementById("links");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
 
 function onLinkClick(clickedId) {
     document.getElementsByClassName('mainSections')[clickedId].scrollIntoView();
